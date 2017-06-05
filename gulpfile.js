6239ab2875c6361e7eb305017c4c877f04d26116
h12345jack <h12345jack@163.com>
2017-06-05 22:58:12 +0800

    add build gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var rename = require('gulp-rename');
 
 gulp.task('default', function() {
   gulp.src("./src/assets/**")
@@ -6,4 +7,15 @@ gulp.task('default', function() {
 });
 
 gulp.task('build', function(){
+  gulp.src("./build/**")
+    .pipe(gulp.dest('./gh-pages'));
+
+  gulp.src("./build/static/js/*.js")
+    .pipe(rename(function(path){
+        path.dirname = ".";
+        path.basename = 'bundle';
+        path.extname = ".js";
+        console.log(path)
+      }))
+      .pipe(gulp.dest('./gh-pages'));
 })
