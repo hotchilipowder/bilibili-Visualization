f36e4bb8f18c9ba5eb42403f350f4ebfeea49b89
h12345jack <h12345jack@bytedance.com>
2017-07-29 17:35:52 +0800

    remove intro react

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import IntroPage from './IntroPage';
 import App from './App';
-
+import './index.less';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
-
-ReactDOM.render(
-  <IntroPage />,
-  document.getElementById('intro-page')
 );
