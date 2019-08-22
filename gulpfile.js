c2278d22afa2e2a7054a8393b1313c6b624ade90
h12345jack <h12345jack@h12345jackdeMacBook-Pro.local>
2019-08-22 14:37:59 +0800

    change to gulp4

--- gulpfile.js
+++ gulpfile.js
@@ -18,45 +18,6 @@ gulp.task('public-watch', function() {
 
 
 
-//gulp build
-
-gulp.task('build',[ 'build_addonvis'], function(){
-
-  return gulp.src("../bilibili-vis-gh-pages/index.html")
-    .pipe(replace(/http:\/\/127\.0\.0\.1:3000/g, 'https://h12345jack.github.io/bilibili-Visualization'))
-    .pipe(rename(function(path){
-      console.log(path);
-    }))
-    .pipe(gulp.dest("../bilibili-vis-gh-pages"))
-
-})
-
-
-gulp.task('build_addonvis',['rename_bundle'],function(){
-    return   gulp.src("../bilibili-vis-gh-pages/assets/js/addonvis.js")
-            .pipe(replace(/http:\/\/127\.0\.0\.1:3000/g, 'https://h12345jack.github.io/bilibili-Visualization'))
-            .pipe(gulp.dest("../bilibili-vis-gh-pages/assets/js/"));
-})
-
-
-gulp.task('rename_bundle',['copy_build'] ,function(){
-  
-    return gulp.src("./build/static/js/*.js")
-        .pipe(rename(function(path){
-            path.dirname = ".";
-            path.basename = 'bundle';
-            path.extname = ".js";
-          }))
-        .pipe(gulp.dest('../bilibili-vis-gh-pages/static/js'));
-});
-
-
-gulp.task('copy_build',['clean'],function(){
-    return gulp.src("./build/**")
-            .pipe(gulp.dest('../bilibili-vis-gh-pages'));
-});
-
-
 gulp.task('clean', function(){
   return gulp.src("../bilibili-vis-gh-pages/static/**",{ read:false })
       .pipe(clean({force: true}));
@@ -68,3 +29,51 @@ gulp.task('cp-gh-pages', function(){
   return gulp.src('./gh-pages/**')
         .pipe(gulp.dest('../bilibili-vis-gh-pages'));
 })
+
+//gulp build
+
+
+
+gulp.task('copy_build', gulp.series('clean' ,function(){
+  return gulp.src("./build/**")
+          .pipe(gulp.dest('../bilibili-vis-gh-pages'));
+}));
+
+
+
+gulp.task('rename_bundle', gulp.series('copy_build' ,function(){
+  
+  return gulp.src("./build/static/js/*.js")
+      .pipe(rename(function(path){
+          path.dirname = ".";
+          path.basename = 'bundle';
+          path.extname = ".js";
+        }))
+      .pipe(gulp.dest('../bilibili-vis-gh-pages/static/js'));
+}));
+
+
+gulp.task('build_addonvis', gulp.series('rename_bundle', function(){
+  return   gulp.src("../bilibili-vis-gh-pages/assets/js/addonvis.js")
+          .pipe(replace(/http:\/\/127\.0\.0\.1:3000/g, 'https://h12345jack.github.io/bilibili-Visualization'))
+          .pipe(gulp.dest("../bilibili-vis-gh-pages/assets/js/"));
+}));
+
+gulp.task('build', gulp.series('build_addonvis', function(){
+
+  return gulp.src("../bilibili-vis-gh-pages/index.html")
+    .pipe(replace(/http:\/\/127\.0\.0\.1:3000/g, 'https://h12345jack.github.io/bilibili-Visualization'))
+    .pipe(rename(function(path){
+      console.log(path);
+    }))
+    .pipe(gulp.dest("../bilibili-vis-gh-pages"))
+
+}));
+
+
+
+
+
+
+
+
